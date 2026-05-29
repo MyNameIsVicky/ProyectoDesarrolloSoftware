4fe9ae90ab57d151379454235946fb1773c70f70
Vicky <justvickyfallas@gmail.com>
2026-05-29 13:27:30 -0600

    Add project files.

---
+++ ProyectoDesarrolloSoftware/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoDesarrolloSoftware.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ProyectoDesarrolloSoftware/Models/Paciente.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoDesarrolloSoftware.Models
+{
+    public class Paciente
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string lastName{ get; set; }
+        [Required]
+        public String nationalId { get; set; }
+        [Required]
+        public int age { get; set; }
+        [Required]
+        public string sex { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        private string password { get; set; }
+
+    }
+}
