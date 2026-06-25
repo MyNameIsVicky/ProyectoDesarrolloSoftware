c4ae8727cf79eb9fcef9d23602ced62b6f4def0a
Isaac Badilla <badillisaac@gmail.com>
2026-06-24 22:02:51 -0600

    cambios

--- ProyectoDesarrolloSoftware/Migrations/20260625033751_dbfinal.Designer.cs
+++ ProyectoDesarrolloSoftware/Migrations/20260625033751_dbfinal.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectoDesarrolloSoftware.Data;
 
@@ -11,9 +12,11 @@ using ProyectoDesarrolloSoftware.Data;
 namespace ProyectoDesarrolloSoftware.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260625033751_dbfinal")]
+    partial class dbfinal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
